65d6458013b72421297dfdb512091387bdae46f2
gsfranca <giovanasantosdefranca634@gmail.com>
2024-09-22 20:59:22 -0300

    app

---
+++ app/src/main/java/com/example/inventory/ui/theme/Color.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.inventory.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_dark_primary = Color(0xFF2E93F2)
+val md_theme_dark_onPrimary = Color(0XFFFFFFFF)
+val md_theme_dark_primaryContainer = Color(0xFF2E93F2)
+val md_theme_dark_onPrimaryContainer = Color(0XFFFFFFFF)
+
+val md_theme_dark_secondary = Color(0xFF347820)
+val md_theme_dark_onSecondary = Color(0XFFFFFFFF)
+val md_theme_dark_secondaryContainer = Color(0xFF347820)
+val md_theme_dark_onSecondaryContainer = Color(0XFFFFFFFF)
+
+val md_theme_dark_tertiary = Color(0xFF4D4D4D)
+val md_theme_dark_onTertiary = Color(0XFFFFFFFF)
+val md_theme_dark_tertiaryContainer = Color(0xFF4D4D4D)
+val md_theme_dark_onTertiaryContainer = Color(0XFFFFFFFF)
+
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF414141)
+val md_theme_dark_onBackground = Color(0xFFFFFFFF)
+
+val md_theme_dark_surface = Color(0xFF4D4D4D)
+val md_theme_dark_onSurface = Color(0xFFFFFFFF)
+
+val md_theme_dark_outline = Color(0xFF6B6B6B)
+
+
+val md_theme_dark_surfaceVariant = Color.Transparent
+val md_theme_dark_onSurfaceVariant = Color.Transparent
+val md_theme_dark_inverseOnSurface = Color.Transparent
+val md_theme_dark_inverseSurface = Color.Transparent
+val md_theme_dark_inversePrimary = Color.Transparent
+val md_theme_dark_surfaceTint = Color.Transparent
+val md_theme_dark_outlineVariant = Color.Transparent
+val md_theme_dark_scrim = Color.Transparent
+
+
+val md_theme_light_primary = md_theme_dark_primary
+val md_theme_light_onPrimary = md_theme_dark_onPrimary
+val md_theme_light_primaryContainer = md_theme_dark_primaryContainer
+val md_theme_light_onPrimaryContainer = md_theme_dark_onPrimaryContainer
+val md_theme_light_secondary = md_theme_dark_secondary
+val md_theme_light_onSecondary = md_theme_dark_onSecondary
+val md_theme_light_secondaryContainer = md_theme_dark_secondaryContainer
+val md_theme_light_onSecondaryContainer = md_theme_dark_onSecondaryContainer
+val md_theme_light_tertiary = md_theme_dark_tertiary
+val md_theme_light_onTertiary = md_theme_dark_onTertiary
+val md_theme_light_tertiaryContainer = md_theme_dark_tertiaryContainer
+val md_theme_light_onTertiaryContainer = md_theme_dark_onTertiaryContainer
+val md_theme_light_error = md_theme_dark_error
+val md_theme_light_errorContainer = md_theme_dark_errorContainer
+val md_theme_light_onError = md_theme_dark_onError
+val md_theme_light_onErrorContainer = md_theme_dark_onErrorContainer
+val md_theme_light_background = md_theme_dark_background
+val md_theme_light_onBackground = md_theme_dark_onBackground
+val md_theme_light_surface = md_theme_dark_surface
+val md_theme_light_onSurface = md_theme_dark_onSurface
+val md_theme_light_surfaceVariant = md_theme_dark_surfaceVariant
+val md_theme_light_onSurfaceVariant = md_theme_dark_onSurfaceVariant
+val md_theme_light_outline = md_theme_dark_outline
+val md_theme_light_inverseOnSurface = md_theme_dark_inverseOnSurface
+val md_theme_light_inverseSurface = md_theme_dark_inverseSurface
+val md_theme_light_inversePrimary = md_theme_dark_inversePrimary
+val md_theme_light_surfaceTint = md_theme_dark_surfaceTint
+val md_theme_light_outlineVariant = md_theme_dark_outlineVariant
+val md_theme_light_scrim = md_theme_dark_scrim
